e9f53ba040a0c702cdbb36c3af1f0e4d170b75c0
Grzegorz <grzegorz@motorro.pl>
2026-09-10 14:19:50 +0200

    fix: stop unconditional redirect on /register

--- apps/web/src/views/unauthorized/index.tsx
+++ apps/web/src/views/unauthorized/index.tsx
@@ -1,20 +1,14 @@
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginPage from './loginPage';
 import RegisterPage from './registerPage';
 import { withPublic } from '@/hocs/withPublic';
-import { useEffect } from 'react';
 
 function Unauthorized() { 
-    const navigate = useNavigate();
-
-    useEffect(() => { 
-        navigate('/login');
-    },[])
-
     return(
         <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
     )
 }
